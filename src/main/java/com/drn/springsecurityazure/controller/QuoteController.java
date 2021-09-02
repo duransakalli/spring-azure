07e72428f327ea7eab6c6aae0d132c5e0c016924
Duran <sakalli.duran@gmail.com>
2021-09-02 12:06:56 -0700

    modified groups

--- src/main/java/com/drn/springsecurityazure/controller/QuoteController.java
+++ src/main/java/com/drn/springsecurityazure/controller/QuoteController.java
@@ -2,6 +2,7 @@ package com.drn.springsecurityazure.controller;
 
 import com.drn.springsecurityazure.service.QuoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +15,21 @@ public class QuoteController {
     private QuoteService quoteService;
 
     @GetMapping("/hello")
+    @PreAuthorize("hasRole('ROLE_group2')")
     public String getMember(Model model) {
         model.addAttribute("welcome", "Welcome to the page");
         return "hello";
     }
 
     @GetMapping("/members/name")
+    @PreAuthorize("hasRole('ROLE_group2')")
     public String getRandomMember(Model model){
         model.addAttribute("name", quoteService.getRandomMember());
         return "name";
     }
 
     @GetMapping("/members")
+    @PreAuthorize("hasRole('ROLE_group2')")
     public String getMembers(Model model) {
         model.addAttribute("members" , quoteService.getAllMembers());
         return "members";
